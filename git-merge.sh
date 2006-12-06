RhettCreighton/git3

commit 9a36c62a8cad0843d4dadad9dcbc4156c6d31ba6

git-merge: fix confusion between tag and branch
In a repository with core.warnambiguousrefs turned off, and with
a branch and a tag that have the same name 'frotz',

	git merge frotz

would merge the commit pointed at by the tag 'frotz' but
incorrectly would identify what was merged as 'branch frotz' in
the merge message.

Signed-off-by: Junio C Hamano <junkio@cox.net>

git-merge.sh
@@ -189,13 +189,13 @@ else
 	merge_name=$(for remote
 		do
 			rh=$(git-rev-parse --verify "$remote"^0 2>/dev/null) &&
-			if git show-ref -q --verify "refs/heads/$remote"
+			bh=$(git show-ref -s --verify "refs/heads/$remote") &&
+			if test "$rh" = "$bh"
 			then
-				what=branch
+				echo "$rh		branch '$remote' of ."
 			else
-				what=commit
-			fi &&
-			echo "$rh		$what '$remote'"
+				echo "$rh		commit '$remote'"
+			fi
 		done | git-fmt-merge-msg
 	)
 	merge_msg="${merge_msg:+$merge_msg$LF$LF}$merge_name"

commit 7e119551ed1415883a0c32f7d348bcf058ff210c

git-merge: fix "fix confusion between tag and branch" for real
An earlier commit 9a36c62a broke the merge message generation with
a careless use of && where it was not needed, breaking the merge
message for cases where non branches are given.

Signed-off-by: Junio C Hamano <junkio@cox.net>

git-merge.sh
@@ -188,8 +188,9 @@ else
 	# in this loop.
 	merge_name=$(for remote
 		do
-			rh=$(git-rev-parse --verify "$remote"^0 2>/dev/null) &&
-			bh=$(git show-ref -s --verify "refs/heads/$remote" 2>/dev/null) &&
+			rh=$(git-rev-parse --verify "$remote"^0 2>/dev/null) ||
+			continue ;# not something we can merge
+			bh=$(git show-ref -s --verify "refs/heads/$remote" 2>/dev/null)
 			if test "$rh" = "$bh"
 			then
 				echo "$rh		branch '$remote' of ."